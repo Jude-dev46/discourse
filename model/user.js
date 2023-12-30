eccf600625198e6b50f20bff3c414980c3cb04b6
Jude Oscar <judeoscar2002gmail.com>
2023-12-30 07:37:22 +0100

    updated model

--- model/user.js
+++ model/user.js
@@ -1,11 +1,12 @@
 const mongoose = require("mongoose");
 const slugify = require("slugify");
+// const validator = require("validator");
 
 const userSchema = new mongoose.Schema({
   refreshToken: { type: String, required: false, unique: true },
   phoneNo: { type: Number, required: true, unique: true },
-  username: { type: String, required: true, unique: true },
-  password: { type: String, required: true, unique: true },
+  username: { type: String, required: true, unique: true, lowercase: true },
+  password: { type: String, required: true, unique: true, maxlength: 8 },
   slug: String,
 });
 
